356e87a8e790a661f29cea6ef343f5ca774cb5e6
Jillian <jillianrosegartner@gmail.com>
2022-08-27 08:22:13 -0700

    added nav, footer $ body files

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
+
+const Footer = () => {
+  return (
+    <div style={styles.container}>
+      <a href="mailto: jillianrosegartner@gmail.com">
+        Email me at jillianrosegartner@gmail.com
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
